145e4eee42c456d0f26b5378bc7ccc549db8bc0c
Yan-Ying Liao <n9102125@gmail.com>
2022-08-30 21:57:10 +0800

    add star repo section

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -6,12 +6,15 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardHeader, Container, Grid, Link, Stack } from "@mui/material";
-import Jkopay from "../../static/jkopay.png";
+import Jkopay from "../../static/jkopay_qrcode.png";
 import Paypal from "../../static/paypal.png";
 import { useTranslation } from "react-i18next";
+import GitHubButton from "react-github-btn";
+import { useTheme } from "@mui/material/styles";
 
 export default function About() {
   const { t } = useTranslation();
+  const theme = useTheme();
   return (
     <Container>
       <Stack spacing={2}>
@@ -38,6 +41,133 @@ export default function About() {
         <Card sx={{ minWidth: 275 }}>
           <CardContent>
             <Typography variant="h5" component="div" gutterBottom>
+              Star Go Germany / VISA Termin Alert
+            </Typography>
+            <Typography variant="body1">
+              If you like this site or find the Termin LINE group helpful, give
+              it a star on Github.
+            </Typography>
+            <ul>
+              <li>
+                Go Germany
+                <ul>
+                  <li>
+                    go-germany：
+                    {theme.palette.mode === "dark" ? (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany"
+                        data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: light_high_contrast;"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    ) : (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    )}
+                  </li>
+                  <li>
+                    go-germany-api：
+                    {theme.palette.mode === "dark" ? (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany-sync"
+                        data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: light_high_contrast;"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany-sync on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    ) : (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany-api"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany-api on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    )}
+                  </li>
+                  <li>
+                    go-germany-sync：
+                    {theme.palette.mode === "dark" ? (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany-sync"
+                        data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: light_high_contrast;"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany-sync on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    ) : (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/go-germany-sync"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/go-germany-sync on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    )}
+                  </li>
+                </ul>
+              </li>
+              <li>
+                VISA Termin Alert
+                <ul>
+                  <li>
+                    visa-termin-alert：
+                    {theme.palette.mode === "dark" ? (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/visa-termin-alert"
+                        data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: light_high_contrast;"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/visa-termin-alert on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    ) : (
+                      <GitHubButton
+                        href="https://github.com/noworneverev/visa-termin-alert"
+                        data-icon="octicon-star"
+                        data-show-count="true"
+                        aria-label="Star noworneverev/visa-termin-alert on GitHub"
+                      >
+                        Star
+                      </GitHubButton>
+                    )}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </CardContent>
+          <CardActions>
+            <Link
+              component={Button}
+              underline="none"
+              target="_blank"
+              rel="noopener"
+              href="https://noworneverev.github.io/"
+            >
+              {t("learnmore")}
+            </Link>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h5" component="div" gutterBottom>
               {t("donate")}
             </Typography>
             <Typography variant="body1" gutterBottom>
@@ -45,16 +175,19 @@ export default function About() {
             </Typography>
             <Grid container>
               <Grid item lg>
-                <Stack direction="row">
+                <Stack direction={{ xs: "column", sm: "row" }}>
                   <Box>
                     <Typography variant="h6">🎁 Paypal me</Typography>
-                    <img src={Paypal} alt="paypal" width="75%" />
+                    <img src={Paypal} alt="paypal" width="60%" />
                   </Box>
                   <Box>
                     <Typography variant="h6">
                       🎁 街口支付 帳號：901578462
                     </Typography>
-                    <img src={Jkopay} alt="jkopay" width="25%" />
+
+                    <Box sx={{ py: 1 }}>
+                      <img src={Jkopay} alt="jkopay" width="65%" />
+                    </Box>
                   </Box>
                 </Stack>
               </Grid>
@@ -83,6 +216,8 @@ export default function About() {
           </CardActions>
         </Card>
       </Stack>
+
+      <Typography></Typography>
     </Container>
   );
 }
