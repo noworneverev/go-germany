05f26a507cce795d1e1c71a9d0c5e3e2059c2fc5
Yan-Ying Liao <n9102125@gmail.com>
2022-08-30 10:24:50 +0800

    add disableShrink to loading component

--- src/app/layout/LoadingComponent.tsx
+++ src/app/layout/LoadingComponent.tsx
@@ -1,10 +1,14 @@
-import { Backdrop, Box, CircularProgress, Typography } from '@mui/material';
+import { Backdrop, Box, CircularProgress, Typography } from "@mui/material";
 
 interface Props {
   message?: string;
+  disableShrink?: boolean;
 }
 
-export default function LoadingComponent({ message = 'Loading...' }: Props) {
+export default function LoadingComponent({
+  message = "Loading...",
+  disableShrink = false,
+}: Props) {
   return (
     <Backdrop open={true} invisible={true}>
       <Box
@@ -13,10 +17,14 @@ export default function LoadingComponent({ message = 'Loading...' }: Props) {
         alignItems="center"
         height="100vh"
       >
-        <CircularProgress size={70} color="inherit" />
+        <CircularProgress
+          size={70}
+          color="inherit"
+          disableShrink={disableShrink}
+        />
         <Typography
           variant="h6"
-          sx={{ justifyContent: 'center', position: 'fixed', top: '60%' }}
+          sx={{ justifyContent: "center", position: "fixed", top: "60%" }}
         >
           {message}
         </Typography>
